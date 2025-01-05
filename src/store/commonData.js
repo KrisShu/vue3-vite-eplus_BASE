1fdddca003d78b35b2d6c24edac007306c2a9eec
KrisShu <1446025061@qq.com>
2025-01-05 14:17:18 +0800

    1

--- src/store/commonData.js
+++ src/store/commonData.js
@@ -25,9 +25,9 @@ export const useCommonStore = defineStore('commonData',{
         // 修改存储中使用的键名称，默认为当前 Store的 id
         key: 'commonData',
         // 修改为 sessionStorage，默认为 localStorage
-        storage: window.localStorage,
+        storage: window.sessionStorage,
         // 部分持久化状态的点符号路径数组，[]意味着没有状态被持久化(默认为undefined，持久化整个状态),把需要持久化的值的key添加进数组即可
-        paths: ['menuIscollapse','need','token','userInfo'],
+        paths: ['need','token','userInfo'],
     },
 
 })
